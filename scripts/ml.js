267c2bba214cbd7c780f3d6ff6b85ca4b2c07b69
Sean McClure <seanaaron100@gmail.com>
2023-10-30 06:23:19 +0000

    adding predicted line3

--- scripts/ml.js
+++ scripts/ml.js
@@ -1,4 +1,10 @@
 setTimeout(function() {
+
+    // Initialize an empty array to store training data
+    let trainingData = { input: [], output: [] };
+
+
+
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     const numEnvironmentBalls = 50;
@@ -76,21 +82,21 @@ setTimeout(function() {
 
     function moveBlueBall(ball) {
         // Apply gravitational force from environment balls
-        for(const envBall of environmentBalls) {
+        for (const envBall of environmentBalls) {
             const dx = envBall.x - ball.x;
             const dy = envBall.y - ball.y;
             const distance = Math.sqrt(dx * dx + dy * dy);
-            if(distance < 3 * ballRadius) {
+            if (distance < 3 * ballRadius) {
                 const angle = Math.atan2(dy, dx);
                 ball.vx += gravitationalForce * Math.cos(angle);
                 ball.vy += gravitationalForce * Math.sin(angle);
             }
         }
         // Bounce off the walls
-        if(ball.x <= ballRadius || ball.x >= maxX - ballRadius) {
+        if (ball.x <= ballRadius || ball.x >= maxX - ballRadius) {
             ball.vx *= -1;
         }
-        if(ball.y <= ballRadius || ball.y >= maxY - ballRadius) {
+        if (ball.y <= ballRadius || ball.y >= maxY - ballRadius) {
             ball.vy *= -1;
         }
         // Update position based on velocity
@@ -101,7 +107,51 @@ setTimeout(function() {
             x: ball.x,
             y: ball.y
         });
+        // Return the current position as the output
+        return {
+            x: ball.x,
+            y: ball.y
+        };
     }
+    
+
+    // Function to collect training data for the machine learning model
+    function collectTrainingData(ball) {
+        const inputPosition = [ball.x, ball.y]; // Current position of the blue ball
+        const outputPosition = moveBlueBall({ ...ball }); // Predicted position of the blue ball
+        trainingData.input.push(inputPosition);
+        trainingData.output.push(outputPosition);
+       // console.log(trainingData.input);
+console.log(trainingData.output);
+
+    }
+
+// Load and train the TensorFlow.js model
+async function loadAndTrainModel() {
+    const model = tf.sequential();
+    model.add(tf.layers.dense({ units: 64, inputShape: [2], activation: 'relu' }));
+    model.add(tf.layers.dense({ units: 2 }));
+
+    model.compile({ optimizer: 'adam', loss: 'meanSquaredError' });
+
+    const inputPositions = trainingData.input;
+    const outputPositions = trainingData.output;
+    const inputTensor = tf.tensor2d(inputPositions, [inputPositions.length, 2]); // [batchSize, inputSize]
+    const outputTensor = tf.tensor2d(outputPositions, [outputPositions.length, 2]); // [batchSize, outputSize]
+
+    // Train the model
+    await model.fit(inputTensor, outputTensor, {
+        epochs: 100,
+        callbacks: {
+            onEpochEnd: (epoch, logs) => {
+                console.log(`Epoch ${epoch + 1}, Loss: ${logs.loss}`);
+            }
+        }
+    });
+
+    // Return the trained model
+    return model;
+}
 
     function predictPath(ball, observationTime) {
         const numSteps = observationTime * 60;
@@ -150,23 +200,18 @@ setTimeout(function() {
             ctx.closePath();
         });
         // Draw paths
-        ctx.strokeStyle = 'blue';
-        ctx.lineWidth = 2;
-        ctx.beginPath();
-        ctx.moveTo(blueBall.path[0].x, blueBall.path[0].y);
-        for(let i = 1; i < blueBall.path.length; i++) {
-            ctx.lineTo(blueBall.path[i].x, blueBall.path[i].y);
-        }
-        ctx.stroke();
-        ctx.closePath();
-        ctx.strokeStyle = 'red';
-        ctx.beginPath();
-        ctx.moveTo(redBall.path[0].x, redBall.path[0].y);
-        for(let i = 1; i < redBall.path.length; i++) {
-            ctx.lineTo(redBall.path[i].x, redBall.path[i].y);
-        }
-        ctx.stroke();
-        ctx.closePath();
+      // Draw paths
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = 2;
+ctx.beginPath();
+if (blueBall.path.length > 0) {
+    ctx.moveTo(blueBall.path[0].x, blueBall.path[0].y);
+    for(let i = 1; i < blueBall.path.length; i++) {
+        ctx.lineTo(blueBall.path[i].x, blueBall.path[i].y);
+    }
+}
+ctx.stroke();
+ctx.closePath();
         // Draw individual balls
         ctx.fillStyle = 'blue';
         ctx.beginPath();
@@ -179,27 +224,54 @@ setTimeout(function() {
         ctx.fill();
         ctx.closePath();
     }
-    setInterval(function() {
-        moveRedBall(redBall);
-        moveBlueBall(blueBall);
-        draw();
-        histogram.line_segment_distribution({
-            individualBalls: [blueBall, redBall]
-        })
-        const blueBallEntropy = calculateEntropy(blueBall.path);
-        const redBallEntropy = calculateEntropy(redBall.path);
-        document.getElementById("title_item_1_2").innerText = blueBallEntropy.toFixed(2);
-        document.getElementById("title_item_2_2").innerText = redBallEntropy.toFixed(2);
-        if(!observationTimeElapsed) {
-            if(blueBall.path.length >= 1 * 60) { // 10 seconds * 60 frames per second
-                observationTimeElapsed = true;
+    async function startPrediction() {
+        const trainedModel = await loadAndTrainModel();
+        let observationTimeElapsed = false;
+    
+        const numFramesForTransition = 30; // Number of frames for the transition
+        let transitionFrameCount = 0;
+        let interpolatedX, interpolatedY;
+    
+        setInterval(function() {
+            moveRedBall(redBall);
+            draw();
+            histogram.line_segment_distribution({
+                individualBalls: [blueBall, redBall]
+            });
+            const blueBallEntropy = calculateEntropy(blueBall.path);
+            const redBallEntropy = calculateEntropy(redBall.path);
+            document.getElementById("title_item_1_2").innerText = blueBallEntropy.toFixed(2);
+            document.getElementById("title_item_2_2").innerText = redBallEntropy.toFixed(2);
+    
+            if (!observationTimeElapsed) {
+                collectTrainingData(blueBall);
+                if (blueBall.path.length >= 10 * 60) { // 10 seconds * 60 frames per second
+                    observationTimeElapsed = true;
+                }
+            } else {
+                // Use the trained model to predict the blue ball's movement
+                const inputPosition = [blueBall.x, blueBall.y];
+                const normalizedPrediction = trainedModel.predict(tf.tensor2d([inputPosition]));
+                const prediction = Array.from(normalizedPrediction.dataSync());
+    
+                // Interpolate the blue ball's position for a smooth transition
+                if (transitionFrameCount < numFramesForTransition) {
+                    interpolatedX = blueBall.x + (prediction[0] - blueBall.x) * (transitionFrameCount / numFramesForTransition);
+                    interpolatedY = blueBall.y + (prediction[1] - blueBall.y) * (transitionFrameCount / numFramesForTransition);
+                    transitionFrameCount++;
+                } else {
+                    interpolatedX = prediction[0];
+                    interpolatedY = prediction[1];
+                }
+    
+                // Update the blue ball's position
+                blueBall.x = interpolatedX;
+                blueBall.y = interpolatedY;
             }
-        } else {
-            const predictedPath = predictPath({
-                ...blueBall
-            }, 10); // Predict for 10 seconds
-            drawPredictedPath(predictedPath);
-        }
-    }, 1000 / 30);
+        }, 1000 / 30);
+    }
+    
+    
+    startPrediction();
     document.querySelector(".lds-roller").style.display = "none";
 }, 2000)
